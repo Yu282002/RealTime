379c01c0f38954cc81fc21d0977200a3f7f67252
Yu282002 <yoshino291@gmail.com>
2025-04-21 01:52:49 +0700

    SimpleSignalR
    simple App Stock Hubs, can follow price

---
+++ SimpleSignalR/SimpleSignalR.Domain/Hubs/StockHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SimpleSignalR.Domain.StockHub
+{
+    public class StockHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("ReceiveMessenge", $"{Context.ConnectionId} has connected");
+        }
+
+        public async Task AskStockPrice(String stock)
+        {
+            await Clients.All.SendAsync($"Receive: Stock: {stock}, Price: {10000}, Volume: {30000}");
+        }
+    }
+}
